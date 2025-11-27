3f48e13774f37e42e558f8c56fcb4e99a7cfff6e
jeeskala-netizen <jeeskala@gmail.com>
2025-11-28 00:46:54 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+TMDB_API_KEY = "92a2bfcd6e2b85e41ca046e16c94ac03" # مفتاحك
+GROQ_API_KEY = "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" # ضع مفتاح Groq الخاص بك هنا
+BASE_URL = "https://api.themoviedb.org/3"
+IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+BACKDROP_URL = "https://image.tmdb.org/t/p/original"
